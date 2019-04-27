cfac425bac3cd230e3ff449305caf12779166be3
kisetsu <17694805702@163.com>
2019-04-27 14:39:50 +0800

    服务器稳定了

---
+++ app/src/main/java/edu/scse/take/MyAppGlideModule.java
@@ -0,0 +1,18 @@
+package edu.scse.take;
+
+/**
+ * TensorFlowTraining
+ *
+ * @author baishixian
+ * @date 2018/3/12 17:52
+ */
+
+import android.widget.TextView;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {
+
+}
